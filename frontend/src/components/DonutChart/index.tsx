2a41bd708a14f31f97a837eeaec35fcd36f3b5f9
Rafael <rafaelenrique30@gmail.com>
2021-11-06 00:48:12 -0300

    DonnutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -9,40 +10,54 @@ type ChartData = {
 }
 
 const DonutChart = () => {
-    
-    //FORMA ERRADA - depois será utilizado o hook
-    let chartData : ChartData = {labels: [], series: []};
 
-    //FORMA ERRADA
+  const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+
+  useEffect(() => {
     axios.get(`${BASE_URL}/sales/amount-by-seller`)
-         .then(response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
+      .then(response => {
+        const data = response.data as SaleSum[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => x.sum);
 
-            chartData = {labels: myLabels , series: mySeries};
-            console.log(response.data);
-         });
+        setChartData({ labels: myLabels, series: mySeries });
+      });
+  }, []);
 
-    //const mockData = {
-    //   series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
-    
-    const options = {
-        legend: {
-            show: true
-        }
+  /*
+  //FORMA ERRADA - depois será utilizado o hook
+  let chartData : ChartData = {labels: [], series: []};
+
+  //FORMA ERRADA
+  axios.get(`${BASE_URL}/sales/amount-by-seller`)
+       .then(response => {
+          const data = response.data as SaleSum[];
+          const myLabels = data.map(x => x.sellerName);
+          const mySeries = data.map(x => x.sum);
+
+          chartData = {labels: myLabels , series: mySeries};
+          console.log(response.data);
+       });
+  */
+  //const mockData = {
+  //   series: [477138, 499928, 444867, 220426, 473088],
+  //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+  //}
+
+  const options = {
+    legend: {
+      show: true
     }
-    
-    return (
-      <Chart 
-        options={{...options, labels: chartData.labels}}
-        series={chartData.series}
-        type="donut"
-        height="240"
-      />  
-    );
+  }
+
+  return (
+    <Chart
+      options={{ ...options, labels: chartData.labels }}
+      series={chartData.series}
+      type="donut"
+      height="240"
+    />
+  );
 }
 
 export default DonutChart;
